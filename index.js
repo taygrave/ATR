bfededcf8dee084ba2cbe9e860b82f347457a024
taygrave <tkhesselgrave@gmail.com>
2017-02-04 23:29:44 -0800

    working now

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
+var secret = require('./secret')
 var Twitter = require('twitter');
 
-// put in secrets here
 var client = new Twitter({
-  consumer_key: '',
-  consumer_secret: '',
-  access_token_key: '',
-  access_token_secret: ''
+  consumer_key: secret.consumer_key,
+  consumer_secret: secret.consumer_secret,
+  access_token_key: secret.access_token_key,
+  access_token_secret: secret.access_token_secret
 });
 
 var params = {screen_name: 'nodejs'};
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
+    console.log('GET TWEETS HERE');
     console.log(tweets);
   }
+});
+
+client.post('statuses/update', {status: 'SO MUCH IN FACT - CATS'},  function(error, tweet, response) {
+  if(error) throw error;
+  console.log('TWEET');
+  console.log(tweet);  // Tweet body.
+  console.log('RESPONSE!!!!');
+  console.log(response);  // Raw response object.
 });
